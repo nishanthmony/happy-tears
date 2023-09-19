a2823ddf8a733c7761a36fbb7914315992867e40
N Nishanth Mony <54764054+nishanthmony@users.noreply.github.com>
2023-09-19 23:55:15 +0530

    homescreen is updated

--- src/components/happyTears.js
+++ src/components/happyTears.js
@@ -3,6 +3,6 @@ import '../styles/happyTears.css'
 
 export const HappyTears = () => {
   return (
-    <div className='marquee'><marquee><b>Happy Tears Surprises</b> is celebrating today 30/09/23, Birthday of the Founder/CEO <b>Nivesh K</b>, use code NIVESHK200 to avail a crazy discount of Rs. 200/- on any Products</marquee></div>
+    <div className='marquee'><marquee><b>Happy Tears Surprises</b> is celebrating today 30/09/23, Birthday of the Founder/CEO <b>Nivesh K</b>, use code NIVESHK200 to avail a crazy discount of Rs. 200/- on selected Products*</marquee></div>
   )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -43,7 +43,7 @@ export const Navbar = ({ user, totalProducts }) => {
                 <div><Link to = 'Login'>Login</Link></div>
             </>}
             {user&&<>
-                <div className='navlink' ><Link className='navlink' to = '/profile'>{user}</Link></div>
+                <div className='navlink' >{user}</div>
                 <div className='cart-menu-btn'>
                     <Link className='navlink' to='/cart'>
                         <Icon icon = {shoppingCart} size = {20} />
